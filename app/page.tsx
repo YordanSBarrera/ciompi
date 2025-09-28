53b51eac01e644dbab3296d185e8ee0cc7c08fd8
Yordan <68411867+YordanSBarrera@users.noreply.github.com>
2025-09-27 21:06:45 -0300

    Revert "add page Cliente"
    This reverts commit 295ddfc047d09838e6e1c86016ebe2b08a94359e.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
+'use client';
+import Image from 'next/image';
+import imgLocal from '@/public/ciompiLocal.webp';
 import { Stack } from '@mui/material';
 import UserLogin from './components/UserLogin';
 
 export default function Home() {
   return (
     <Stack position="relative" width="100%" height="70vw">
+      <Image
+        src={imgLocal}
+        alt="img del local"
+        fill
+        // style={{
+        //   objectFit: 'cover',
+        //   objectPosition: 'center',
+        // }}
+        style={{
+          objectFit: 'contain',
+          objectPosition: 'center',
+          width: '100%',
+          height: '100%',
+        }}
+      />
       <UserLogin />
     </Stack>
   );
